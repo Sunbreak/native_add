3f82adc661c6f6e2244468814a90d7e576f4d6e4
Sunbreak <sunbreak.wang@gmail.com>
2020-04-07 00:14:40 +0800

    Add `native_add` function
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- lib/native_add.dart
+++ lib/native_add.dart
@@ -1,6 +1,16 @@
 import 'dart:async';
+import 'dart:ffi';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+
+final DynamicLibrary nativeAddLib = Platform.isAndroid
+    ? DynamicLibrary.open("libnative_add.so")
+    : DynamicLibrary.process();
+
+final int Function(int x, int y) nativeAdd = nativeAddLib
+    .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add")
+    .asFunction();
 
 class NativeAdd {
   static const MethodChannel _channel = const MethodChannel('native_add');
